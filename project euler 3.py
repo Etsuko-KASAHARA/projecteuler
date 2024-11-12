58bd1ab3ef54ae037620e4a4f4632fef0f488c26
Ecchan <hi23etsuko03jap@gmail.com>
2024-11-12 18:45:08 +0900

    need review

--- project euler 3.py
+++ project euler 3.py
@@ -26,28 +26,50 @@ def prime_factor (x):
 def prime_factor2 (x):
     divisor= 2
     next_number = x
-    result_list=[]
+    result = 0
     while divisor <= x:
         if next_number % divisor == 0:
             next_number = round(next_number / divisor)
-            result_list.append(divisor)
+            result = divisor
         
         divisor+= 1
-    return result_list   
+    return result  
 
 
-def prime_sum(prime_factors):
-    result = 0
-    for factor in prime_factors:
-        result += factor
-    return result    
+
+
 
 print(prime_factor2(60)) 
-print(prime_factor2(13195)) 
-print(prime_sum(prime_factor2(13195)))
+print(prime_factor2(13195))
 
 ## too big number
-##print(prime_factor2(600851475143))
-##print(prime_sum(prime_factor2(600851475143)))  
+##print(prime_factor2(600851475143)) 
+
+def prime_factor3(x):
+    next_number = x
+    divisor = 2
+    result=0
+    
+    while next_number % divisor == 0:
+        next_number = round(next_number / divisor)
+        result = devisor
+
+    divisor = 3    
+
+    while divisor * divisor <= next_number:
+        while next_number % divisor == 0:
+            next_number = round(next_number / divisor)
+            result = divisor
+        divisor += 2
+
+    if next_number > 2:
+        result = next_number
+
+    return result
+
+
+print(prime_factor3(13195))
+print(prime_factor3(600851475143))
+
 
 
