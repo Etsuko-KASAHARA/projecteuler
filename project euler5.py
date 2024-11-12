4aec472bc2ab9f79fa5c3b8da36a350df80256c9
Ecchan <hi23etsuko03jap@gmail.com>
2024-11-12 21:55:16 +0900

    infinite roop

--- project euler5.py
+++ project euler5.py
@@ -1,22 +1,19 @@
 ##2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder
 ##what is the smallest positive number that is evenly dividible by all of the numbers from 1 to 20?
 
+"""
 def number(y):
-
     result = 0
-    
-    for x in range(1,y):
+    for x in range(2520,y):
         divisor = 1
-        
         if all(x % divisor == 0 for divisor in range(1, 20)):
             result = x
             return result
             
-
+"""
          
 
 
-
-print(number (100000))       
-
+##infinite rooppppppppppp
+##print(number (10000000))       
 
