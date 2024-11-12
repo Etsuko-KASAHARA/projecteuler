0a7d990d91dbf2e08c969b0787f14f307f05e7be
Ecchan <hi23etsuko03jap@gmail.com>
2024-11-12 20:19:10 +0900

    [::-1]

---
+++ project euler 4.py
@@ -0,0 +1,24 @@
+## A palindromic number reads the same both ways
+## The largest palindrome made from the product of
+##2-digit numbers is 9009 = 91 ×99
+##Find the largest palindrome made from the product of two 3-digit numbers
+
+
+
+def palindrome_number (a):
+    
+    for num1 in range(999, 99, -1):
+        for num2 in range(999, 99, -1):
+            result = str(num1 * num2)
+            if result == result[::-1]:
+                a.append(int(result))
+    return max(a) 
+
+
+a = []
+print(palindrome_number (a))     
+
+##b = 1234
+##print((str(b))[::-1])
+##print((str(b))[::-2])
+##print((str(b))[0:3:2])
