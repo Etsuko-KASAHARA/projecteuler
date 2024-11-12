853a943c42640c4d1a1abec88cb7d4681f065e03
Ecchan <hi23etsuko03jap@gmail.com>
2024-11-12 22:39:26 +0900

    muy dificil

--- project euler5.py
+++ project euler5.py
@@ -17,3 +17,33 @@ def number(y):
 ##infinite rooppppppppppp
 ##print(number (10000000))       
 
+"""
+def number(y):
+    
+    result = 0
+    
+    for num in range(y, 1000000, -1):
+        divisor = 1
+        if all(num % divisor == 0 for divisor in range(1,20)):
+            result = num
+            return result 
+
+print(number(10000000))   
+"""
+
+
+
+
+
+
+##これだと違うんだよねぇ
+"""
+def number():
+
+    total=1
+    for n in range(20,0,-1):
+        total *= n
+    return total   
+
+print(number())
+"""
