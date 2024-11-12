b23e4c7bbf893f3a1e17eb27df8c24d72fd68722
Ecchan <hi23etsuko03jap@gmail.com>
2024-11-12 10:13:46 +0900

    problem1

--- project euler 1.py
+++ project euler 1.py
@@ -1,2 +1,25 @@
-x = 1
-print(x)
+##if we list the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.abs
+# # the sum of these multiples is 23.
+# # Find the sum of all the multiples of 3 or 5 below 1000 
+
+
+
+
+def sum (x):
+    result = 0
+    first_num = 0
+    while first_num < x:
+        
+            if first_num % 3 == 0 or first_num % 5 == 0:
+                
+                result += first_num
+                first_num += 1
+            else:
+                first_num += 1
+
+        
+            
+    return (result)
+    
+
+print(sum(1000))    
